edf4a61447232c67caa03888c8d0fef3eff10e99
Rafael Aragao Morais <ram.morais50@gmail.com>
2024-10-06 17:24:16 -0400

    Add crash test
    Signed-off-by: Rafael Aragao Morais <ram.morais50@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Iniciando o programa..." << std::endl;
+
+    std::cout << "Executando tarefas..." << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+
+    std::cout << "Programa finalizado após 30 segundos." << std::endl;
     return 0;
 }
